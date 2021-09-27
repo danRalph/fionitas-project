e5285ceba283115d28ab96e820af6b21a36ab191
danRalph <64211348+danRalph@users.noreply.github.com>
2021-09-27 21:02:36 +0100

    Update stripe.js

--- backend/constants/stripe.js
+++ backend/constants/stripe.js
@@ -1,11 +1,8 @@
 const configureStripe = require('stripe');
 require('dotenv').config();
 
-// const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-//     ? 'sk_live_MY_SECRET_KEY'
-//     : 'sk_test_u2U6RqevSJh8VpJvWOMuHLbG00IzzEkEkv';
 
-const STRIPE_SECRET_KEY = 'sk_test_XXXXXXXJh8VpJvWOMuHLbG00IzzEkEkv';
+const STRIPE_SECRET_KEY = 'sk_test_XXXXXXXJh8VpJvWOXXXXjXG00IzzEkEkv';
 
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
