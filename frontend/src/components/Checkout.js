9b076ccc08dec744c068fcee814621d902ea35cc
danRalph <64211348+danRalph@users.noreply.github.com>
2021-09-27 21:00:49 +0100

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -3,21 +3,9 @@ import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 
-
-// const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-//   ? 'http://myapidomain.com'
-//   : 'http://localhost:7000/payments';
-
 const PAYMENT_SERVER_URL = 'http://localhost:7000/payments';
 
-
-// const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-//   ? 'pk_live_2n807FMXM9RyJUxezcA8BgzZ00wj9IWmjH'
-//   : 'pk_test_hi3lnBo8kCVHQLbZeXLrN4gx00Qs5Zc35n';
-
-
-
-const STRIPE_PUBLISHABLE = 'pk_test_hXXXXXXCVHQLbZeXLrN4gx00Qs5Zc35n';
+const STRIPE_PUBLISHABLE = 'pk_test_hXXXXXXCVHQLbZeXLrXXgx00Qs5Zc35n';
 
 
 const CURRENCY = 'GBP';
